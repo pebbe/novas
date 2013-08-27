ff0344bb2bc950c5461b71691ee9ee399af8c5b4
Peter Kleiweg <p.c.j.kleiweg@rug.nl>
2013-08-27 12:19:14 +0200

    README: suggestion on what DE set to use.

--- doc.go
+++ doc.go
@@ -10,5 +10,7 @@ from this site:
 ftp://ssd.jpl.nasa.gov/pub/eph/planets/fortran/
 
 and follow the instructions in userguide.txt
+
+Those instructions tell you to also download a set of DE files. With set? I don't know. DE405 seems to work fine.
 */
 package novas
